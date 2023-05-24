0e00b71e226acdbbd13fe3782071feb821660730
TejasMishra29 <mtejas292002@gmail.com>
2023-05-25 00:22:29 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+import  webapp2
+
+class Mainpage(webapp2.RequestHandler):
+    def get(self):
+        
+        self.response.write('Hello World!')
+app = webapp2.WSGIApplication([('/' , Mainpage)] , debug = True)
